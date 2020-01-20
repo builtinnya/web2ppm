814b4fb89eea9fc24fddac2cf798b75cc78dd083
Naoto Yokoyama <builtinnya@gmail.com>
2020-01-21 01:03:39 +0900

    :tada: implement web2ppm

--- cmd/web2ppm/main.go
+++ cmd/web2ppm/main.go
@@ -1,42 +1,38 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"io/ioutil"
-	"log"
-	"math"
-	"fmt"
-	"os"
-	"github.com/spf13/cobra"
 	"github.com/chromedp/cdproto/emulation"
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
+	"github.com/spakin/netpbm"
+	"github.com/spf13/cobra"
+	"image/png"
+	"log"
+	"math"
+	"os"
 )
 
 func fullScreenshot(urlstr string, quality int64, res *[]byte) chromedp.Tasks {
 	return chromedp.Tasks{
 		chromedp.Navigate(urlstr),
 		chromedp.ActionFunc(func(ctx context.Context) error {
-			// get layout metrics
 			_, _, contentSize, err := page.GetLayoutMetrics().Do(ctx)
 			if err != nil {
 				return err
 			}
-
 			width, height := int64(math.Ceil(contentSize.Width)), int64(math.Ceil(contentSize.Height))
 
-			// force viewport emulation
 			err = emulation.SetDeviceMetricsOverride(width, height, 1, false).
 				WithScreenOrientation(&emulation.ScreenOrientation{
 					Type:  emulation.OrientationTypePortraitPrimary,
 					Angle: 0,
-				}).
-				Do(ctx)
+				}).Do(ctx)
 			if err != nil {
 				return err
 			}
 
-			// capture screenshot
 			*res, err = page.CaptureScreenshot().
 				WithQuality(quality).
 				WithClip(&page.Viewport{
@@ -55,7 +51,7 @@ func fullScreenshot(urlstr string, quality int64, res *[]byte) chromedp.Tasks {
 }
 
 func execRootCmd(cmd *cobra.Command, args []string) error {
-	url := args[0]
+	urlstr := args[0]
 
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.DisableGPU,
@@ -64,15 +60,25 @@ func execRootCmd(cmd *cobra.Command, args []string) error {
 
 	allocCtx, cancelAlloc := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancelAlloc()
-	ctx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	ctx, cancel := chromedp.NewContext(allocCtx)
 	defer cancel()
 
-	var buf []byte
-	if err := chromedp.Run(ctx, fullScreenshot(url, 90, &buf)); err != nil {
-		log.Fatal(err)
+	var pngBuf []byte
+	if err := chromedp.Run(ctx, fullScreenshot(urlstr, 90, &pngBuf)); err != nil {
+		return err
 	}
-	if err := ioutil.WriteFile("screenshot.png", buf, 0644); err != nil {
-		log.Fatal(err)
+
+	img, err := png.Decode(bytes.NewReader(pngBuf))
+	if err != nil {
+		return err
+	}
+
+	err = netpbm.Encode(os.Stdout, img, &netpbm.EncodeOptions{
+		Format: netpbm.PPM,
+		Plain:  true,
+	})
+	if err != nil {
+		return err
 	}
 
 	return nil
@@ -80,10 +86,10 @@ func execRootCmd(cmd *cobra.Command, args []string) error {
 
 func newCmd() *cobra.Command {
 	rootCmd := &cobra.Command{
-		Use:   "web2ppm",
-		Short: "Takes a screenshot of a web page as Portable Pixel Map",
-		Args: cobra.ExactArgs(1),
-		RunE: execRootCmd,
+		Use:   "web2ppm [flags] <url>",
+		Short: "Takes a screenshot of an entire web page and outputs to stdout as Plain Portable Pixel Map",
+		Args:  cobra.ExactArgs(1),
+		RunE:  execRootCmd,
 	}
 	return rootCmd
 }
@@ -91,7 +97,6 @@ func newCmd() *cobra.Command {
 func main() {
 	rootCmd := newCmd()
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 }
